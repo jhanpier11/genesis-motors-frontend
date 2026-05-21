0ab3a8bcc54187ce7be0fbe971dff67efd23a0d0
Yajaira <yajaira_vasquezramos@hotmail.com>
2026-05-21 04:12:35 -0500

    Usar URL de Render directamente en api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:3000/api';
+const API_URL = 'https://genesis-motors-backend-9nr6.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
